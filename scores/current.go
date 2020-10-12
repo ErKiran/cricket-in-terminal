28b03667500ee352bec6abca88275899b8ce926e
ErKiran <kiruu1238@gmail.com>
2020-10-12 23:31:28 +0545

    Working on MiniScoreCard Section

--- scores/current.go
+++ scores/current.go
@@ -11,18 +11,58 @@ func Current(url string) []utils.CurrentTopic {
 }
 
 func Commentary(url, id string) {
+	// for range time.Tick(time.Second * 1) {
 	comm := utils.GetCommentaryUpdates(url, id)
 	var comments []string
 
-	for _, comment := range comm.CommentaryList {
-		if len(comment.CommentaryFormats.Bold.FormatValue) != 0 {
-			comments = append(comments, fmt.Sprintf("🏏🏏 %v\n", strings.Replace(comment.CommText, comment.CommentaryFormats.Bold.FormatID[0], comment.CommentaryFormats.Bold.FormatValue[0], -1)))
-		} else {
-			comments = append(comments, fmt.Sprintf("🏏🏏 %v\n", comment.CommText))
+	for _, inning := range comm.Miniscore.MatchScoreDetails.InningsScoreList {
+		if inning.InningsID == 1 {
+			fmt.Printf("First Innings %v %d-%d(%.1f)", inning.BatTeamName, inning.Score, inning.Wickets, inning.Overs)
 		}
+		// js, _ := json.MarshalIndent(inning, "", " ")
+		// fmt.Println(string(js), "MiniScore")
+	}
+
+	fmt.Println(comm.Miniscore.MatchScoreDetails.CustomStatus)
+
+	for _, comment := range comm.CommentaryList {
+		event := comment.Event
+		var emoji string
+		if event == "NONE" {
+			event = ""
+		}
+
+		if event == utils.FOUR || event == utils.SIX {
+			emoji = "🏏"
+		}
+
+		if event == utils.OVER_BREAK {
+			emoji = "⏳"
+		}
+
+		if event == utils.WICKET {
+			emoji = "🇼"
+		}
+
+		if len(comment.CommentaryFormats.Bold.FormatValue) != 0 {
+			comments = append(comments, fmt.Sprintf("%v  %v %v\n", emoji, event, strings.Replace(comment.CommText, comment.CommentaryFormats.Bold.FormatID[0], comment.CommentaryFormats.Bold.FormatValue[0], -1)))
+		} else {
+			comments = append(comments, fmt.Sprintf("%v  %v %v\n", emoji, event, comment.CommText))
+		}
+
+		// batsman := fmt.Sprintf(`%v %v (%v) [(%d) 6's, (%d) 4's]`, comment.BatsmanStriker.BatName, comment.BatsmanStriker.BatRuns, comment.BatsmanStriker.BatBalls, comment.BatsmanStriker.BatSixes, comment.BatsmanStriker.BatFours)
+		// bowler := fmt.Sprintf(`%v %v-%v(%.1f)`, comment.BowlerStriker.BowlName, comment.BowlerStriker.BowlWkts, comment.BowlerStriker.BowlRuns, comment.BowlerStriker.BowlOvs)
+
+		// for _, innings := range comment.{
+		// 	fmt.Println(innings)
+		// }
+
+		// fmt.Println(batsman)
+		// fmt.Println(bowler)
 	}
 
 	for _, comment := range comments {
 		fmt.Println(comment)
 	}
+	// }
 }
---
+++ utils/constants.go
@@ -0,0 +1,9 @@
+package utils
+
+const (
+	WICKET       = "WICKET"
+	OVER_BREAK   = "over-break"
+	FOUR         = "FOUR"
+	SIX          = "SIX"
+	TEAM_HUNDRED = "TEAM_HUNDRED"
+)
